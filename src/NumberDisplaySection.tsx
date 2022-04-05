793c62d9a9c61750105157315bfd82a15302e42d
Pravin Bashyal <pravin.bashyal@signavio.com>
2022-04-05 14:49:43 +0200

    add number display section

---
+++ src/NumberDisplaySection.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const NumberDisplaySection = ({ numbers }: { numbers: string[] }) => (
+  <section
+    aria-label="number input section"
+    style={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+  >
+    {numbers.map((digit, i) => (
+      <NumberItem digit={digit} key={`${digit}-${i}`}></NumberItem>
+    ))}
+  </section>
+);
+
+const NumberItem = ({ digit }: { digit: string }) => {
+  return (
+    <p
+      style={{
+        height: "2rem",
+        width: "2rem",
+        padding: "1rem",
+        margin: "1rem",
+        border: "1px solid rgba(50,50,50,0.5)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {digit}
+    </p>
+  );
+};
