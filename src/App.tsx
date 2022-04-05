76779f9023b400662f6ad1cee6af1e3cb91f1d1e
Pravin Bashyal <pravin.bashyal@signavio.com>
2022-04-05 14:52:57 +0200

    use refactored sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,40 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import { NumberDisplaySection } from "./NumberDisplaySection";
+import { NumberInputSection } from "./NumberInputSection";
 
-const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => String(num));
+export const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => String(num));
 
-function App() {
+export default function App() {
+  const { splittedNumber, addDigitToNumber } = useNumber();
+
+  return (
+    <section className="App">
+      <NumberDisplaySection numbers={splittedNumber}></NumberDisplaySection>
+      <NumberInputSection
+        addDigitToNumber={addDigitToNumber}
+      ></NumberInputSection>
+    </section>
+  );
+}
+
+const useNumber = () => {
   const [splittedNumber, setSplittedNumber] = useState<string[]>([]);
+  const addDigitToNumber = (digit: string) => {
+    setSplittedNumber((prevNum) => [...prevNum, digit]);
+  };
 
   useEffect(() => {
     const onKeyDown = (e: KeyboardEvent) => {
       if (digits.includes(e?.key)) {
-        console.log("is digit");
-        setSplittedNumber((prevNum) => [...prevNum, e.key]);
+        addDigitToNumber(e.key);
       }
     };
 
     document.addEventListener("keydown", onKeyDown);
   }, []);
-
-  return (
-    <section className="App">
-      <NumberInputSection numbers={splittedNumber}></NumberInputSection>
-    </section>
-  );
-}
-
-export default App;
-
-const NumberItem = ({ digit }: { digit: string }) => {
-  return (
-    <p
-      style={{
-        height: "2rem",
-        width: "2rem",
-        padding: "1rem",
-        margin: "1rem",
-        border: "1px solid rgba(50,50,50,0.5)",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      {digit}
-    </p>
-  );
+  return {
+    splittedNumber,
+    addDigitToNumber,
+  };
 };
-
-const NumberInputSection = ({ numbers }: { numbers: string[] }) => (
-  <section
-    aria-label="number input section"
-    style={{ display: "flex", alignItems: "center", justifyContent: "center" }}
-  >
-    {numbers.map((digit, i) => (
-      <NumberItem digit={digit} key={`${digit}-${i}`}></NumberItem>
-    ))}
-  </section>
-);
