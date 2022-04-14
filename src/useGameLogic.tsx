1e128d6107a28f2ca5f3184489a225b0ea52e3b6
Pravin Bashyal <pravin.bashyal@signavio.com>
2022-04-14 17:05:48 +0200

    check dual occurences

--- src/useGameLogic.tsx
+++ src/useGameLogic.tsx
@@ -1,43 +1,21 @@
 import { useState } from "react";
-import { ANumber } from "./useNumber";
-
-export type NumberLog = {
-  number: string;
-  correctDigitCount: number;
-  correctPositionCount: number;
-};
 
 const generatedNumber = "1234";
-
-const checkCorrectness = (splittedNumber: string[], numberToCheck: string) => {
-  let correctDigitCount = 0;
-  let correctPositionCount = 0;
-
-  numberToCheck.split("").forEach((digit: string, i) => {
-    if (splittedNumber.includes(digit)) {
-      correctDigitCount += 1;
-    }
-    if (splittedNumber.findIndex((num) => num === digit) === i) {
-      correctPositionCount += 1;
-    }
-  });
-  return { correctPositionCount, correctDigitCount };
-};
 
 export function useGameLogic() {
   const [numbersLog, setNumbersLog] = useState<Array<NumberLog>>([]);
   const [isCorrectNumber, setIsCorrectNumber] = useState(false);
 
-  const checkNumber = (guessedNumber: string[]) => {
+  const checkNumber = (userInputNumber: string[]) => {
     const { correctDigitCount, correctPositionCount } = checkCorrectness(
-      guessedNumber,
+      userInputNumber,
       generatedNumber
     );
-    setIsCorrectNumber(guessedNumber.length === correctPositionCount);
+    setIsCorrectNumber(userInputNumber.length === correctPositionCount);
     setNumbersLog((prev) => [
       ...prev,
       {
-        number: guessedNumber.join(""),
+        number: userInputNumber.join(""),
         correctDigitCount,
         correctPositionCount,
       },
@@ -46,3 +24,48 @@ export function useGameLogic() {
 
   return { numbersLog, checkNumber, isCorrectNumber };
 }
+
+const checkCorrectness = (
+  userInputNumber: string[],
+  generatedNumber: string
+) => {
+  let correctDigitCount = 0;
+  let correctPositionCount = 0;
+
+  let userInputNumberCopy = userInputNumber;
+  generatedNumber.split("").forEach((digit: string) => {
+    if (userInputNumberCopy.includes(digit)) {
+      correctDigitCount += 1;
+      userInputNumberCopy = withoutFirstOccurence({
+        of: digit,
+        from: userInputNumberCopy,
+      });
+    }
+  });
+
+  generatedNumber.split("").forEach((digit: string, i) => {
+    if (userInputNumber.findIndex((num) => num === digit) === i) {
+      correctPositionCount += 1;
+    }
+  });
+  return { correctPositionCount, correctDigitCount };
+};
+
+export type NumberLog = {
+  number: string;
+  correctDigitCount: number;
+  correctPositionCount: number;
+};
+
+const withoutFirstOccurence = ({
+  of: digitToCheck,
+  from: numbersArray,
+}: {
+  of: string;
+  from: Array<string>;
+}) => {
+  let isAlreadyDeleted = false;
+  return numbersArray.filter(
+    (digit) => isAlreadyDeleted || digit !== digitToCheck
+  );
+};
